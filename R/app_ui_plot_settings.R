7fb452250ec494375f8033e76a86ac0f7e9e8540
katcom <13948357+katcom@users.noreply.github.com>
2024-01-19 19:10:43 +0800

    add hr line for line width and size

--- R/app_ui_plot_settings.R
+++ R/app_ui_plot_settings.R
@@ -7,28 +7,29 @@ plot_settings_ui <- function(id) {
         selectInput(ns("x"), "X axis",choices=list()),
         selectInput(ns("y"), "Y axis",choices=list()),
         selectInput(ns("z"), "Z axis",choices=list()),
+        hr(),
         sliderInput(ns("scale_factor"), "Scale Factor", value = 1, min = 1, max = 10),
         sliderInput(ns("line_width"), "Edge Width Factor", value = 3, min = 1, max = 10),
         hr(),
         fluidRow(
-          column(5,p('Show Grid')),
+          column(6,p('Show Grid')),
           column(3,Toggle.shinyInput(ns("show_grid"), value = TRUE))
         ),
         fluidRow(
-          column(5,p('Show Zero Lines')),
+          column(6,p('Show Zero Lines')),
           column(3,Toggle.shinyInput(ns("show_zeroline"), value = TRUE))
         ),
         hr(),
         fluidRow(
-          column(5,p('Show X-axis Arrow')),
+          column(6,p('Show X-axis Arrow')),
           column(3,Toggle.shinyInput(ns("show_x_axis_arrow"), value = TRUE))
         ),
         fluidRow(
-          column(5,p('Show Y-axis Arrow')),
+          column(6,p('Show Y-axis Arrow')),
           column(3,Toggle.shinyInput(ns("show_y_axis_arrow"), value = TRUE))
         ),
         fluidRow(
-          column(5,p('Show Z-axis Arrow')),
+          column(6,p('Show Z-axis Arrow')),
           column(3,Toggle.shinyInput(ns("show_z_axis_arrow"), value = TRUE))
         ),
 
